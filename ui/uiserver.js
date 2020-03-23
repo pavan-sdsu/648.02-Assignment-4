72ea8a757544b8527e09f003559dcb2bb76388cd
pavan <pavan1645@gmail.com>
2020-03-22 23:21:49 -0700

    eslint bug fixes

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 8000;
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(PORT, () => console.log("Listening on PORT", PORT));
+app.listen(PORT, () => console.log('Listening on PORT', PORT));
